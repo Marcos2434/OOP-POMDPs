b30a2653994baa0212a33a4e65a270e4bff165ca
Marcos Bauch Mira <marcosbauch2434@gmail.com>
2024-03-07 22:10:17 +0100

    added customization and aesthetics

--- visualizer/app/static/app/js/index.js
+++ visualizer/app/static/app/js/index.js
@@ -1,14 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello World");
+configs = {
+    white: {
+        canvas_bg: "rgb(255, 255, 255)",
+        node_bg: "rgb(255, 255, 255)",
+        node_border: "rgb(0, 0, 0)",
+        node_highlight_bg: "rgb(255, 255, 255)",
+        node_highlight_border: "rgb(0, 0, 0)",
+        node_font_color: "rgb(0, 0, 0)",
+    },
+    transparent: {
+        canvas_bg: "rgb(47, 46, 51)",
+        node_bg: "rgb(47, 46, 51)",
+        node_border: "rgb(255, 255, 255)",
+        node_highlight_bg: "rgb(47, 46, 51)",
+        node_highlight_border: "rgb(255, 255, 255)",
+        node_font_color: "rgb(255, 255, 255)",
+    }
+}
 
+config = configs.transparent;
+
+document.addEventListener("DOMContentLoaded", () => {
     const mobileNav = document.querySelector(".hamburger");
     mobileNav.addEventListener("click", () => toggleNav());
 
     handleNetwork();
-    // handleNetwrokContainer();
+    handleNetwrokContainerColor();
+    
+    document.querySelector("#theme-select").addEventListener("change", e => {
+        config = configs[e.target.value];
+        handleNetwork();
+        handleNetwrokContainerColor();
+    });
 })
 
-const handleNetwrokContainer = () => {
+const handleNetwrokContainerColor = () => {
+    const network_canvas = document.querySelector("#network");
+    network_canvas.style.backgroundColor = config.canvas_bg;
+}
+
+const aspectRatio = () => {
     const network_canvas = document.querySelector("#network");
     const styles = getComputedStyle(network_canvas);
     network_canvas.style.height = styles.width;
@@ -17,16 +47,16 @@ const handleNetwrokContainer = () => {
 
 const handleNetwork = () => {
     // create an array with nodes
-    var nodes = new vis.DataSet([
-        { id: 1, label: "Node 1" },
-        { id: 2, label: "Node 2" },
-        { id: 3, label: "Node 3" },
-        { id: 4, label: "Node 4" },
-        { id: 5, label: "Node 5" },
+    const nodes = new vis.DataSet([
+        { id: 1, label: "1"},
+        { id: 2, label: "2"},
+        { id: 3, label: "3"},
+        { id: 4, label: "4"},
+        { id: 5, label: "5"},
     ]);
 
     // create an array with edges
-    var edges = new vis.DataSet([
+    const edges = new vis.DataSet([
         { from: 1, to: 3 },
         { from: 1, to: 2 },
         { from: 2, to: 4 },
@@ -35,13 +65,57 @@ const handleNetwork = () => {
     ]);
 
     // create a network
-    var container = document.getElementById("network");
-    var data = {
+    const container = document.getElementById("network");
+    const data = {
         nodes: nodes,
         edges: edges,
     };
-    var options = {};
-    var network = new vis.Network(container, data, options);
+    const options = {
+        autoResize: true,
+        height: '100%',
+        width: '100%',
+        locale: 'en',
+        clickToUse: false,
+        configure: {},    // defined in the configure module.
+        edges: {},        // defined in the edges module.
+        nodes: {
+            shape: "dot",
+            size: 20,
+            scaling: {
+                label: {
+                    min: 10,  // minimum font size
+                    max: 30   // maximum font size
+                }
+            },
+            font: {
+                size: 20,
+                face: 'Tahoma',
+                color: config.node_font_color,
+            }
+        },        // defined in the nodes module.
+        // groups: {},       // defined in the groups module.
+        // layout: {},       // defined in the layout module.
+        // manipulation: {}, // defined in the manipulation module.
+        interaction: {
+            dragView: false,
+            zoomView: false,
+        },  // defined in the interaction module.
+        physics: {},      // defined in the physics module.
+    };
+
+    const network = new vis.Network(container, data, options);
+    
+    for (node of nodes.get()) {
+        node.color = {
+            border: config.node_border,  // Color of the border
+            background: config.node_bg,  // Color of the background
+            highlight: {  // Colors when the node is selected
+                border: config.node_highlight_border,  // Color of the border
+                background: config.node_highlight_bg,  // Color of the background
+            }
+        }
+        nodes.update(node)
+    }
 }
 
 const toggleNav = () => {
